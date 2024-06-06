530f7ea90aeb63e6e3599a70061b4589baa0f489
Paulo <prsgomes88@gmail.com>
2024-06-06 16:48:14 -0300

    Adicionando passos para executar os testes no CI e Adicionando o checkout ao pipeline

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,13 +3,12 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from sqlalchemy.pool import StaticPool
 
 from fast_zero.app import app
 from fast_zero.database import get_session
 from fast_zero.models import User, table_registry
-from fast_zero.settings import Settings
 from fast_zero.security import get_password_hash
+from fast_zero.settings import Settings
 
 
 class UserFactory(factory.Factory):
